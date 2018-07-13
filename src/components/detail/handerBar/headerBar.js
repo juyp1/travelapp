83f595957c1283e0495643b47af3f36741f81b74
juyp1 <76542009@qq.com>
2018-07-13 22:15:38 +0800

    update

--- src/components/detail/handerBar/headerBar.js
+++ src/components/detail/handerBar/headerBar.js
@@ -1,8 +1,32 @@
 export default {
   name: 'headerBar',
+  data () {
+    return {
+      showAbs: false,
+      opacityeStyle: {
+        opactity: 0
+      }
+    };
+  },
+  activated () {
+    window.addEventListener('scroll', this.handlesSroll);
+  },
   methods: {
     back () {
       this.$router.push('/');
+    },
+    handlesSroll () {
+      const top = document.documentElement.scrollTop;
+      if (top > 80) {
+        let opacity = top / 140;
+        opacity = opacity > 1 ? 1 : opacity;
+        this.opacityeStyle = {
+          opacity
+        };
+        this.showAbs = true;
+      } else {
+        this.showAbs = false;
+      }
     }
   }
 };
